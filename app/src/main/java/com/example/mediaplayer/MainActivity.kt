5264b5ed2c12e7db1a45236515326a890578ce0c
Douglas <douglasfon@192.168.1.2>
2023-04-21 13:00:22 -0400

    add imagens, logic mainActivity

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.mediaplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnPlay: Button
+    private lateinit var btnPause: Button
+    private lateinit var btnStop: Button
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        btnPlay = findViewById(R.id.btnPlay)
+        btnPause = findViewById(R.id.btnPause)
+        btnStop = findViewById(R.id.btnStop)
+
+        mediaPlayer = MediaPlayer.create(application, R.raw.bach)
+
+        btnPlay.setOnClickListener {
+                if (mediaPlayer != null) {
+                    mediaPlayer.start()
+                }
+            }
+
+        btnPause.setOnClickListener {
+            if (mediaPlayer != null && mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+            }
+        }
+
+        btnStop.setOnClickListener {
+            if (mediaPlayer != null && mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+                mediaPlayer.prepare()
+            }
+        }
+        }
 }
