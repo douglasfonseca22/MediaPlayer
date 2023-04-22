6aecbfaba493df54c424f5486b10c9c3c2bf6fae
Douglas <douglasfon@192.168.1.2>
2023-04-22 11:12:13 -0400

    feature volume

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.mediaplayer
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnPlay: Button
     private lateinit var btnPause: Button
     private lateinit var btnStop: Button
+    private lateinit var seekBar: SeekBar
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var audioManager: AudioManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,14 +24,40 @@ class MainActivity : AppCompatActivity() {
         btnPlay = findViewById(R.id.btnPlay)
         btnPause = findViewById(R.id.btnPause)
         btnStop = findViewById(R.id.btnStop)
+        seekBar = findViewById(R.id.seekBar)
 
         mediaPlayer = MediaPlayer.create(application, R.raw.bach)
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        val volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        seekBar.max = volumeMaximo
+
+        val volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        seekBar.progress = volumeAtual
+
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                audioManager.setStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    progress,
+                    AudioManager.FLAG_SHOW_UI
+                )
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Do Nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Do Nothing
+            }
+        })
 
         btnPlay.setOnClickListener {
-                if (mediaPlayer != null) {
-                    mediaPlayer.start()
-                }
+            if (mediaPlayer != null) {
+                mediaPlayer.start()
             }
+        }
 
         btnPause.setOnClickListener {
             if (mediaPlayer != null && mediaPlayer.isPlaying) {
@@ -40,5 +71,20 @@ class MainActivity : AppCompatActivity() {
                 mediaPlayer.prepare()
             }
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mediaPlayer != null && mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mediaPlayer != null && mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+    }
 }
